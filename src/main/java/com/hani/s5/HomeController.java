34e063dd4a2dfc97512743471eecdb7b55388972
SOOKYOUNG-KIM <63095409+SOOKYOUNG-KIM@users.noreply.github.com>
2020-05-07 09:27:28 +0900

    commit

--- src/main/java/com/hani/s5/HomeController.java
+++ src/main/java/com/hani/s5/HomeController.java
@@ -23,7 +23,7 @@ public class HomeController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	
-	@Value("${oracle.user2}")
+	@Value("${oracle.user}")
 	private String user;
 	
 	
